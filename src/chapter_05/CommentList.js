4fa451b305ce8d6e1de0e3ad96bd1f5524022f1f
LimHaeJun <6462jun@gmail.com>
2023-04-06 11:20:34 +0900

    map 함수로 받아와서 출력하기

--- src/chapter_05/CommentList.js
+++ src/chapter_05/CommentList.js
@@ -1,12 +1,41 @@
 import React from "react";
 import Comment from "./Comment"
 
+//function CommentList(props) {
+   // return (
+     //   <div>
+       //     <Comment name={"임해준"} comment={"안녕하세요. 임해준입니다."} />
+       //     <Comment name={"임해준2"} comment={"안녕하세요. 임해준2입니다."} />
+       //     <Comment name={"임해준3"} comment={"안녕하세요. 임해준3입니다."} />
+       // </div>
+   // )
+//}
+
+const comments = [
+    {
+        name: "임해준",
+        comment: "안녕하세요. 임해준입니다."
+    },
+    {
+        name: "임해준",
+        comment: "안녕하세요. 임해준입니다."
+    },
+
+{
+    name: "임해준",
+    comment: "안녕하세요. 임해준입니다."
+},
+
+]
+
 function CommentList(props) {
     return (
         <div>
-            <Comment name={"임해준"} comment={"안녕하세요. 임해준입니다."} />
-            <Comment name={"임해준2"} comment={"안녕하세요. 임해준2입니다."} />
-            <Comment name={"임해준3"} comment={"안녕하세요. 임해준3입니다."} />
+            {comments.map((foo) => {
+                return (
+                    <Comment name={foo.name} comment={foo.comment} />
+                )
+            })}
         </div>
     )
 }
