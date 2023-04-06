6e351c2fc7c88d680f269c4e760d9d15dadb68c8
LimHaeJun <6462jun@gmail.com>
2023-04-06 11:14:06 +0900

    댓글 컴포넌트2

--- src/chapter_05/CommentList.js
+++ src/chapter_05/CommentList.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Comment from "./Comment"
 
-function CommenList(props) {
+function CommentList(props) {
     return (
         <div>
-            <Comment />
+            <Comment name={"임해준"} comment={"안녕하세요. 임해준입니다."} />
+            <Comment name={"임해준2"} comment={"안녕하세요. 임해준2입니다."} />
+            <Comment name={"임해준3"} comment={"안녕하세요. 임해준3입니다."} />
         </div>
     )
 }
 
-export default CommentList
+
+export default CommentList;
