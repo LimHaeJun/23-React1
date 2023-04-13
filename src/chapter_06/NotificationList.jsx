755e7729ea26abeca73353a0c9540ebe468936a1
LimHaeJun <6462jun@gmail.com>
2023-04-13 16:26:16 +0900

    6주차 실습

--- src/chapter_06/NotificationList.jsx
+++ src/chapter_06/NotificationList.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import Notification from "./Notification";
 
 const reservedNotifications = [
-    {
+    {   
+        id: 1,
         message: "안녕하세요, 오늘 일정을 알려드립니다."
     },
 
-    {
+    {   
+        id: 2,
         message: "점심 식사 시간입니다."
     },
 
-    {
+    {   
+        id: 3,
         message: "이제 곧 미팅이 시작됩니다."
     },
 ];
@@ -21,7 +24,7 @@ class NotificationList extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {
+        this.state = { //notifications 비우기!
             notifications: [],
         };
     }
@@ -45,7 +48,10 @@ render() {
     return (
         <div>
             {this.state.notifications.map((notification) => {
-                return <Notification message={notification.message} />;
+                return <Notification 
+                key={notification.id}
+                id={notification.id}
+                message={notification.message} />;
             })}
         </div>
         );
